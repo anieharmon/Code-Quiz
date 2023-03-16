1cdbd8bd0a8d5645877d1e186e29e56e002581cb
anieharmon <anieharmon@icloud.com>
2023-03-16 17:55:08 -0600

    started code

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//var anie = "anie"
+var quizButton = document.querySelector("#startQuiz");
+
+quizButton.addEventListener("click",function(){
+document.querySelector("#questionView").style.display = "block"
+document.querySelector("#introView").style.display = "none"
+})
